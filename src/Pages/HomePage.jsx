56ac7eda5d1bb9804f4465ce482ac98953243de2
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-10-13 21:37:41 -0500

    Setup project with folders and the start of the homepage

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <h2 className="text-center text-3xl">The Board</h2>
+    </div>
+  )
+}
+
+export default HomePage
