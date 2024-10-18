00a2f98477833e27037f42b21b963b0f430c7492
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2024-10-18 14:14:04 -0400

    Had to install react-router-dom to create the login page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
 import HomePage from "./Pages/HomePage";
 
 function App() {
     return (
-        <>
-            <HomePage />
-        </>
+        <Router>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </Router>
     );
 }
 
