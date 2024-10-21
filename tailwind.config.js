962274708dcd96ad67e8e10d9680174f8e111cab
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-10-21 18:25:32 -0500

    Added colors and font to Tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,23 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightPurple: "#B48DEE", 
+        blue: "#78CFFF", 
+        lightBlue: "#C1EEFF",
+        yellow: "#FFCB50",
+        gray: "#4C4E69", 
+        orange: "#B45700",
+        teal: "#02616E",
+        darkPurple: "#750075",
+        darkGray: "#252525",
+      },
+      fontFamily: {
+        font: ["Questrial", "sans-serif"],
+      },
   },
+},
   plugins: [],
 }
 
